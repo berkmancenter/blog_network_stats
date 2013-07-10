a9b369971ed95a229d7d3e65009735817f0991f6
Tomas Reimers <tomasreimers@gmail.com>
2013-07-10 16:32:28 -0400

    Revert to test again

--- blog_network_stats.php
+++ blog_network_stats.php
@@ -151,8 +151,8 @@ class Blog_network_class {
                 "<a href='" . get_blog_details($blog)->path . "'>" . get_blog_option($blog, "blogname") . "</a>",
                 "<div class='directory_description' title='" . get_blog_option($blog, "blogdescription") . "'>" . get_blog_option($blog, "blogdescription") . "</div>",
                 $blogusers_string,
-                */
                 date("n/j/Y", strtotime(get_blog_details($blog)->registered)),
+                */
                 date("n/j/Y", strtotime(get_blog_details($blog)->last_updated))
             );
 
