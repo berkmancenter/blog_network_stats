993674b67b90882c11569787753500cff9eb2693
Tomas Reimers <tomasreimers@gmail.com>
2013-07-11 14:26:25 -0400

    Also set it to echo at end

--- blog_network_stats.php
+++ blog_network_stats.php
@@ -163,9 +163,7 @@ class Blog_network_class {
             if ($blog == 2107 || $blog == 50){
                 fwrite($json_file, memory_get_usage(true));
                 fwrite($json_file, "\n");
-                fwrite($json_file, json_encode(get_defined_vars()));
-                fwrite($json_file, "\n");
-                fwrite($json_file, json_encode(get_defined_functions()));
+                fwrite($json_file, var_export(get_defined_vars(), true));
                 fwrite($json_file, "\n");
             }
 
@@ -179,6 +177,8 @@ class Blog_network_class {
 
         }
 
+        fwrite($json_file, var_export(get_defined_vars(), true));
+        
         // fwrite($json_file, ']}');
 
         fclose($json_file);
