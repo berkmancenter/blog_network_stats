2cd370a12b6e96f69bed67f62ef1cc36bbc887ca
Tomas Reimers <tomasreimers@gmail.com>
2013-07-11 12:43:32 -0400

    Echo defined vars within loop - with real usage

--- blog_network_stats.php
+++ blog_network_stats.php
@@ -161,7 +161,7 @@ class Blog_network_class {
             // fwrite($json_file, json_encode($row));
 
             if ($blog == 2107 || $blog == 50){
-                fwrite($json_file, memory_get_usage());
+                fwrite($json_file, memory_get_usage(true));
                 fwrite($json_file, "\n");
                 fwrite($json_file, json_encode(get_defined_vars()));
                 fwrite($json_file, "\n");
